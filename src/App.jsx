3bb47730a0f2d042bebdcf34e1c7736d0ee3b6c9
Sergi Florensa <sflorensadev@gmail.com>
2025-06-14 14:53:16 +0200

    Hook funcional con crida a la API realizado con buscador i geolocalización

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,71 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useWeather } from "./hooks/useWeather";
+import './App.css'
 
-
-function App() {
-  const [count, setCount] = useState(0)
+export default function App() {
+  const {
+    location,
+    setLocation,
+    weather,
+    loading,
+    error,
+    fetchByLocation,
+    fetchByGeo,
+  } = useWeather();
 
   return (
-    <>
-      
-    </>
+    <div className="app-container">
+
+      <div className="nav">
+        <button
+          onClick={fetchByGeo}
+        >
+          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="currentColor">
+            <path fillRule="evenodd" d="m7.539 14.841.003.003.002.002a.755.755 0 0 0 .912 0l.002-.002.003-.003.012-.009a5.57 5.57 0 0 0 .19-.153 15.588 15.588 0 0 0 2.046-2.082c1.101-1.362 2.291-3.342 2.291-5.597A5 5 0 0 0 3 7c0 2.255 1.19 4.235 2.292 5.597a15.591 15.591 0 0 0 2.046 2.082 8.916 8.916 0 0 0 .189.153l.012.01ZM8 8.5a1.5 1.5 0 1 0 0-3 1.5 1.5 0 0 0 0 3Z" clipRule="evenodd" />
+          </svg>
+
+        </button>
+        <input
+          type='text'
+          value={location}
+          onChange={(e) => setLocation(e.target.value)}
+          placeholder='Escriu una ciutat... '
+
+        ></input>
+        <button
+          onClick={fetchByLocation}
+          disabled={loading}
+        >
+          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="currentColor">
+            <path fillRule="evenodd" d="M9.965 11.026a5 5 0 1 1 1.06-1.06l2.755 2.754a.75.75 0 1 1-1.06 1.06l-2.755-2.754ZM10.5 7a3.5 3.5 0 1 1-7 0 3.5 3.5 0 0 1 7 0Z" clipRule="evenodd" />
+          </svg>
+
+        </button>
+      </div>
+
+
+      {loading ? (
+        <div className="loading-screen">
+          <p>Carregant dades del temps...</p>
+        </div>
+      ) : error ? (
+        <div>
+
+        </div>
+      ) : weather ? (
+        <div className="weather">
+          <div className='weather-card'>
+
+            <img src={weather.current.condition.icon} alt="Weatehr Icon" />
+
+            <p>{Math.round(weather.current.temp_c)}ºC</p>
+            <p>{Math.round(weather.current.feelslike_c)}ºC</p>
+            <p>{Math.round(weather.current.humidity)}%</p>
+          </div>
+        </div>
+      ) : null}
+
+    </div>
+
   )
 }
 
-export default App
---
+++ src/hooks/useWeather.js
@@ -0,0 +1,84 @@
+import { useState } from "react";
+import axios from "axios";
+
+export function useWeather() {
+  const [location, setLocation] = useState("");
+  const [weather, setWeather] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState("");
+
+  const API_KEY = import.meta.env.VITE_WEATHER_API;
+
+  const fetchByLocation = async () => {
+    if (!location) return;
+    try {
+      setError("");
+      setLoading(true);
+      console.log(API_KEY);
+      const res = await axios.get(
+        `https://api.weatherapi.com/v1/forecast.json`,
+        {
+          params: {
+            key: API_KEY,
+            q: location,
+            days: 7,
+            lang: "ca",
+          },
+        }
+      );
+      console.log(res.data);
+      setWeather(res.data);
+    } catch {
+      setError("Ciutat no trobada.");
+      setWeather(null);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  const fetchByGeo = () => {
+    if (!navigator.geolocation) {
+      setError("Geolocalització no suportada.");
+      return;
+    }
+
+    navigator.geolocation.getCurrentPosition(
+      async (pos) => {
+        const { latitude, longitude } = pos.coords;
+        try {
+          setError("");
+          setLoading(true);
+          const res = await axios.get(
+            `https://api.weatherapi.com/v1/forecast.json`,
+            {
+              params: {
+                key: API_KEY,
+                q: `${latitude},${longitude}`,
+                days: 7,
+                lang: "ca",
+              },
+            }
+          );
+          setWeather(res.data);
+          setLocation(res.data.location.name);
+        } catch {
+          setError("No s'ha pogut carregar la localització.");
+          setWeather(null);
+        } finally {
+          setLoading(false);
+        }
+      },
+      () => setError("Permís rebutjat.")
+    );
+  };
+
+  return {
+    location,
+    setLocation,
+    weather,
+    loading,
+    error,
+    fetchByLocation,
+    fetchByGeo,
+  };
+}
